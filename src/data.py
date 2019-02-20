f1d5bf53b8a98e23b0f77f149be0c7caad30bf5e
md <mihkal@dunfjeld.no>
2019-02-20 14:52:07 +0100

    Various data structures

---
+++ src/data.py
@@ -0,0 +1,36 @@
+linux_image_list = [
+    'CentOS 6 (1809) x86_64',                      
+    'CentOS 7.3 x86_64',                             
+    'CentOS 7.5 x86_64',                             
+    'CirrOS 0.3.4 x86_64',                           
+    'Debian 8.6.0 (Jessie) stable amd64',
+    'Debian 8.7.0 (Jessie) stable amd64',            
+    'Debian 9.0.0 (Stretch) stable amd64',           
+    'Debian 9.4.2 (Stretch) stable amd64',           
+    'Kali Linux 2016.1 xfce amd64',                  
+    'Kali Linux 2017.3 xfce amd64',                  
+    'Kali Linux 2018.2 xfce amd64',                  
+    'Kali Linux 2018.3a xfce amd64',                 
+    'Puppet Learning VM 2017.3.5 6.5',               
+    'Ubuntu Server 14.04 LTS (Trusty Tahr) i386',
+    'Ubuntu Server 16.04 LTS (Xenial Xerus) amd64',  
+    'Ubuntu Server 16.04 LTS (Xenial Xerus) i386',   
+    'Ubuntu Server 16.10 (Yakkety Yak) amd64',       
+    'Ubuntu Server 16.10 (Yakkety Yak) i386',      
+    'Ubuntu Server 17.04 (Zesty Zapus) amd64',     
+    'Ubuntu Server 18.04 LTS (Bionic Beaver) amd64',
+    'Ubuntu Server 18.04 LTS (Bionic Beaver) i386', 
+    'Ubuntu Server 18.10 (Cosmic Cuttlefish) amd64',
+    'openSUSE Leap 42.3 x86_64'
+]
+
+windows_image_list = [
+    'Windows 10 1803 Enterprise Eval',            
+    'Windows 10 1803 Professional N',              
+    'Windows Server 2012 R2 Standard',               
+    'Windows Server 2012 R2 Std Eval',               
+    'Windows Server 2016 Standard',                  
+    'Windows Server 2016 Std Eval',                  
+    'Windows Server 2019 Standard',                  
+    'Windows Server 2019 Standard Evaluation'
+]
