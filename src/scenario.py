bccb550ee65d1ba5950060c6768a9e8eb14b3128
md <mihkal@dunfjeld.no>
2019-02-26 21:55:39 +0100

    Make sure scenarios can only be created with resources valid for that specific scenario type

--- src/scenario.py
+++ src/scenario.py
@@ -13,17 +13,38 @@ class Scenario(object):
     def __init__(self, data):
         self.data = data
         self.template = self.get_scenario_template()
+        self.type = self.data['scenario']['type']
         self.platform = self.check_platform(self.data)
         self.allocated_subnets = []
         self.node_list = []
         self.router_list = []
+
         self.initialize_scenario()
 
     def initialize_scenario(self):
+        if self.scenario_resources_is_valid(self.data) is False:
+            print('Invalid resource(s) specified for scenario type', self.type)
+            print('Valid resource(s) for', self.type, 'are', self.get_valid_types(self.type))
+            sys.exit(1)
         self.heat_instantiate(self.data)
 
     def get_template(self):
         return self.template
+
+    def get_valid_types(self, stype):
+        foo = {
+            'jeopardy': ['vulnerability'],
+            'attack-defense': ['node', ],
+            'redteam-vs-blueteam': ['node', 'router', 'service', 'team', 'agent', 'vulnerability', 'user', ''],
+            'wargame': ['service', 'vulnerability']
+        }
+        return foo[stype]
+
+    def scenario_resources_is_valid(self, data):
+        for resource in data['resources']:
+            if data['resources'][resource]['type'] not in self.get_valid_types(self.type):
+                return False
+        return True
 
     def check_platform(self, data):   
         """Check which backend platform is to be used. Added for future needs"""
@@ -36,7 +57,7 @@ class Scenario(object):
             platform = 'azure'
             raise NotImplementedError
         return platform
-    
+
     def scenario_type_is_valid(self):
         """ Verify correct scenario type"""
         return True if self.data['scenario']['type'].lower() in \
@@ -47,7 +68,7 @@ class Scenario(object):
         """Opens the appropriate heat template based on scenario type"""
         if self.scenario_type_is_valid():
             type = str(self.data['scenario']['type']).lower()
-            path = os.path.join('lib', type + '-template.yaml')
+            path = os.path.join('lib', 'scenario-templates', type + '-template.yaml')
         else:
             print('Invalid scenario type')
             sys.exit(1)
