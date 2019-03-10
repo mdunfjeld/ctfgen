bffcd6cbc7ac7ca434d1d7075e9826dc2ee269cd
md <mihkal@dunfjeld.no>
2019-03-10 13:41:03 +0100

    Update: A defined node is now assigned to each team

--- src/scenario.py
+++ src/scenario.py
@@ -19,28 +19,38 @@ class Scenario(object):
         self.node_list = []
         self.router_list = []
 
-        self.initialize_scenario()
+        self.initialize_scenario(self.type, self.data)
 
-    def initialize_scenario(self):
-        if self.scenario_resources_is_valid(self.data) is False:
-            print('Invalid resource(s) specified for scenario type', self.type)
-            print('Valid resource(s) for', self.type, 'are', self.get_valid_types(self.type))
+    def initialize_scenario(self, stype, data):
+        if self.scenario_resources_are_valid(data) is False:
+            print('Invalid resource(s) specified for scenario type', stype)
+            print('Valid resource(s) for', stype, 'are', self.get_valid_types(stype))
             sys.exit(1)
-        self.heat_instantiate(self.data)
+
+        if self.type == 'jeopardy':
+            self.jeopardy_create(data)
+        elif self.type == 'wargame':
+            self.wargame_create(data)
+        elif self.type == 'attack-defense':
+            self.attack_defense_create(data)
+        else:
+            self.redteam_blueteam_create(data)
 
     def get_template(self):
         return self.template
 
     def get_valid_types(self, stype):
+        """Get a list of valid resource objects for the specified scenario type"""
         foo = {
             'jeopardy': ['vulnerability'],
-            'attack-defense': ['node', ],
-            'redteam-vs-blueteam': ['node', 'router', 'service', 'team', 'agent', 'vulnerability', 'user', ''],
-            'wargame': ['service', 'vulnerability']
+            'attack-defense': ['node', 'vulnerability', 'team', 'agent', 'objective' ],
+            'redteam-vs-blueteam': ['node', 'router', 'service', 'team', 'agent', 'vulnerability', 'user', 'objective', 'rules'],
+            'wargame': ['service', 'vulnerability', 'node']
         }
         return foo[stype]
 
-    def scenario_resources_is_valid(self, data):
+    def scenario_resources_are_valid(self, data):
+        """Verify that valid objects are specfied for the desired scenario type"""
         for resource in data['resources']:
             if data['resources'][resource]['type'] not in self.get_valid_types(self.type):
                 return False
@@ -75,7 +85,29 @@ class Scenario(object):
         with open(path) as file:
             return yaml.load(file)
     
-    def heat_instantiate(self, data):
+    def jeopardy_create(self, data):
+        pass
+
+    def wargame_create(self, data):
+        pass
+    
+    def get_teams(self, data):
+        for team in data['resources']:
+            if data['resources'][team]['type'] == 'team':
+                yield team, data['resources'][team]
+
+    def attack_defense_create(self, data):
+        for team_name, team_data in self.get_teams(data): # Is team_data needed?
+            for device_name in data['resources']:
+                if data['resources'][device_name]['type'] == 'node':
+                    node = Node(
+                        data['resources'][device_name], 
+                        '{}_{}'.format(team_name, device_name), 
+                        self.template
+                    )
+                    self.node_list.append(node)
+
+    def redteam_blueteam_create(self, data):
         """Create the heat infrastructure"""
         for device_name in data['resources']:
             if data['resources'][device_name]['type'] == 'router':
@@ -87,6 +119,7 @@ class Scenario(object):
                 )
                 self.router_list.append(router)
                 self.allocated_subnets = router.get_allocated_subnets()
+        for device_name in data['resources']:
             if data['resources'][device_name]['type'] == 'node':
                 node = Node(
                     data['resources'][device_name], 
@@ -94,5 +127,5 @@ class Scenario(object):
                     self.template
                 )
                 self.node_list.append(node)
-
-    
+            #elif data['resources'][device_name]['type'] == 'vulnerability':
+            #    v = Vulnerability()
