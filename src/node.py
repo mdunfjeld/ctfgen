dbca6d0ff61ffd3774ab39d18a20e27c05900012
md <mihkal@dunfjeld.no>
2019-03-13 21:00:11 +0100

    Add metadata field for nodes

--- src/node.py
+++ src/node.py
@@ -7,12 +7,13 @@ from src.helpers import debug_yaml  # For debugging only
 from src.helpers import prettyprint # For debugging only
 
 class Node(object):
-    # Remember to test if port_list, net and subnet works properly
-    def __init__(self, data, node_name, template, port_list=None, net=None, subnet=None):
+    # Remember to test if port_list, net and subnet works properly.
+    def __init__(self, data, node_name, template, ansible_group=None, port_list=None, net=None, subnet=None):
         self.data = data
         self.node_name = node_name
         self.port_list = port_list
         self.net = net
+        self.ansible_group = ansible_group
         self.subnet = subnet
         self.template = template
         self.subnet_count = self.count_subnets()
@@ -27,11 +28,21 @@ class Node(object):
                 router = self.data['properties']['networks'][idx]['router']
                 subnet = self.data['properties']['networks'][idx]['subnet']
                 self.add_node_ports(idx, router, subnet)
+        else:
+            self.add_node_ports('0', 'management', 'attack_defense_subnet') # TODO: fix other types as well
 
         if 'public_ip' in self.data['properties'].keys() and self.data['properties']['public_ip'] is True:
             self.add_floating_ip()
 
+    def add_outputs(self):
+        self.template['outputs'].update(OrderedDict({
+            str(self.node_name + '_ip'): {
+                'value': ""
+            }
+        }))
+
     def count_subnets(self): 
+        """Get the number of subnets the node is connected to"""
         if 'properties' not in self.data.keys() or self.data['properties'] is None:
             return 0
         elif 'networks' not in self.data['properties'].keys():
@@ -50,6 +61,7 @@ class Node(object):
             yield (port_number, subnet_name[0], subnet_name[1])
 
     def set_flavor(self):
+        """Set the Node's flavor"""
         if 'flavor' not in self.data['properties'].keys():
             return 'm1.small'
         elif self.data['properties']['flavor'].lower() not in flavor_list:
@@ -59,6 +71,7 @@ class Node(object):
             return str(self.data['properties']['flavor'])
     
     def set_operating_system(self):
+        """Set the Node's operating system"""
         if 'os' not in self.data['properties'].keys():
             return 'Ubuntu Server 18.04 LTS (Bionic Beaver) amd64'      # Default OS unless specified
         elif self.check_os_family() is None:
@@ -68,6 +81,7 @@ class Node(object):
             return str(self.data['properties']['os'])
 
     def check_os_family(self):
+        """Check whether the specified OS is linux or windows based"""
         os = str(self.data['properties']['os']).lower()
         for image in windows_image_list:
             if os in str(image).lower():
@@ -78,9 +92,10 @@ class Node(object):
         return None
 
     def add_node_ports(self, idx, router, subnet):
+        """Create and add OS::Neutron::Port resource(s) to the heat template"""
         if router == 'management' and subnet == 'attack_defense_subnet':
             subnet_id  = { 'get_attr': ['management', subnet ]}
-            network_id = { 'get_attr': ['management', 'attack_defense_net']}
+            network_id = { 'get_attr': ['management', 'attack_defense_net']} # Should change naming of predefined static network
         else:
             subnet_id  = { 'get_resource': subnet }
             network_id = { 'get_resource': str(router + 'net')}
@@ -97,8 +112,6 @@ class Node(object):
             }}
         })
         self.template['resources'].update(port)
-
-
 
     def add_node(self, ports):
         """Create the OS::Nova::Server resource and the relevant parameters"""
@@ -118,6 +131,11 @@ class Node(object):
                 'type': 'OS::Nova::Server',
                # 'depends on': 'management',
                 'properties': {
+                    'config_drive': 'true',
+                    'personality': {
+                        'pubkey': { 'get_file': '../ansible_deploy_key.pub'},
+                    },
+                    'metadata': { 'group': self.ansible_group },
                     'name': { 'get_param': self.node_name + '_server_name' },
                     'image': { 'get_param': self.node_name + '_image' },
                     'flavor': { 'get_param': self.node_name + '_flavor' },
@@ -180,26 +198,27 @@ class Node(object):
     def add_security_group(self, idx, subnet):
         """Adds a security group resource to the template"""
         # ICMP is always allowed.
-        rule_list = [
-            OrderedDict({
-                'remote_ip_prefix': '0.0.0.0/0',
-                'protocol': 'icmp'
-            })]
+        rule_list = [ OrderedDict({'remote_ip_prefix': '0.0.0.0/0', 'protocol': 'icmp'}) ]
+
         subnet = self.data['properties']['networks'][idx]['subnet']
         resource_name = str(self.node_name + '_security_group_' + subnet)
 
+        # Port security is not specified
         if 'port_security' not in self.data['properties']['networks'][idx].keys():
             rule_list.append(self.create_portsecurity_rule(22, 'tcp'))
-           # return rule_list
+
+        # Port security is specified
         else:
+            # Port security and TCP is specified
             if 'tcp' in self.data['properties']['networks'][idx]['port_security'].keys():
-                # All servers must have SSH even if not specified
+                # Add SSH if it's not specified
                 if 22 not in self.data['properties']['networks'][idx]['port_security']['tcp']:
                     rule_list.append(self.create_portsecurity_rule(22, 'tcp'))
 
                 for tcp_port in self.data['properties']['networks'][idx]['port_security']['tcp']:
                     rule_list.append(self.create_portsecurity_rule(tcp_port, 'tcp'))
 
+            # Add SSH if Port security is specified but TCP is not. 
             else:
                 rule_list.append(self.create_portsecurity_rule(22, 'tcp'))
 
@@ -207,7 +226,7 @@ class Node(object):
                 for udp_port in self.data['properties']['networks'][idx]['port_security']['udp']:
                     rule_list.append(self.create_portsecurity_rule(udp_port, 'udp'))
 
-            # Security group Heat resource
+        # Security group Heat resource
         secgrp = OrderedDict({
             resource_name: {
             'type': 'OS::Neutron::SecurityGroup',
@@ -219,12 +238,14 @@ class Node(object):
         return resource_name
 
     def add_software_config(self): # It should be possible to select which files are used. Need to fix this.
+        """This one probably needs more work"""
         config = OrderedDict({
                 'str_replace': {
-                    'template': { 'get_file': '../lib/testing/foo.sh' },
+                    'template': { 'get_file': '../lib/scripts/ubuntu_boot.sh' },
                     'params': {
-                        '__puppet_master_ip__': { 'get_attr': ['management', 'puppet_master_ip']}
+                        '__manager_ip__': { 'get_attr': ['management', 'manager_ip']}
                     }
                 }
         })
         return config
+
